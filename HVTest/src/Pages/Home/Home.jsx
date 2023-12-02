8b331fb9b4e72aa24ade356ab13bda37a9899445
badr <bernardoantoniod@gmail.com>
2023-12-02 12:11:23 -0300

    figma home second section

--- HVTest/src/Pages/Home/Home.jsx
+++ HVTest/src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom"
+import { BsInstagram, BsLinkedin, BsWhatsapp} from "react-icons/bs";
 import Layout from "../../Components/Layout/Layout"
 import GenericCardServices from "../../Components/GenericCardServices/GenericCardServices"
 import "./Home.css"
@@ -7,12 +8,23 @@ const Home = () => {
     return (
         <Layout>
           <section id="home" className="home__main--container">
-            <section className="main--title--container">
-              <h1 className="home__main--title">Servicios de Ingeniería de Calidad para la Industria</h1>
+            <section className="home__social__logos__container">
+              <a className="footer__solcial__logo" href="https://www.instagram.com/hvtest.cl/" target="_blank" rel="noopener noreferrer">
+                <BsInstagram className="insta-logo"/>
+              </a>
+              <a className="footer__solcial__logo" href="https://www.linkedin.com/company/hv-test" target="_blank" rel="noopener noreferrer">
+                  <BsLinkedin className="linke-logo"/>
+              </a>
+              <a className="footer__solcial__logo" href="" target="_blank" rel="noopener noreferrer">
+                  <BsWhatsapp className="whatsapp-logo"/>
+              </a>
             </section>
-            <section className="home__main-buttons-container">
-              <button>Contactanos</button>
-              <button>Nuestros Proyectos</button>
+            <section className="main--title--container">
+              <h1 className="home__main--title">Servicios <b className="styling__word">de Ingeniería</b> <br></br> de Calidad para la Industria</h1>
+              <section className="home__main-buttons-container">
+                <button className="home__main--button--left">Contactanos</button>
+                <button className="home__main--button--rigth">Ver Proyectos</button>
+              </section>
             </section>
           </section>
           <section className="home__bottom">
