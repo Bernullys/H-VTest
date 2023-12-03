5bec4338e4121b66f33b290816e6b6cf3bd0b910
badr <bernardoantoniod@gmail.com>
2023-12-02 22:15:00 -0300

    figma footer

--- HVTest/src/Components/Footer/Footer.jsx
+++ HVTest/src/Components/Footer/Footer.jsx
@@ -7,123 +7,55 @@ const Footer = () => {
 
     return (
         <footer>
-            <nav className="footer__nav--container">
+            <section className="footer__full__container">
+                <section className="footer__nav--container">
+                    <section className="footer__central__logo__container">
+                        <NavLink className="a-logo-spa" to="/">
+                            <img className="logo__hvt--main--footer" src="../../../src/assets/LogoHVTest&diagnosticsSpa.png" alt="HVTest-company-logo" />
+                        </NavLink>
+                    </section>
+                    <section className="footer__central__container">
+                        <ul className="footer__nav__ul--social">
+                            <a className="footer__solcial__logo" href="https://www.instagram.com/hvtest.cl/" target="_blank" rel="noopener noreferrer">
+                                <BsInstagram className="footer__insta-logo"/>
+                            </a>
+                            <a className="footer__solcial__logo" href="https://www.linkedin.com/company/hv-test" target="_blank" rel="noopener noreferrer">
+                                <BsLinkedin className="footer__linke-logo"/>
+                            </a>
+                            <a className="footer__solcial__logo" href="" target="_blank" rel="noopener noreferrer">
+                                <BsWhatsapp className="footer__whatsapp-logo"/>
+                            </a>
+                        </ul>
 
-                <ul className="footer__nav__ul">
-                    <h3>Enlaces</h3>
+                        <ul className="footer__nav__ul">
+                            <h3>Email</h3>
+                            <li>bernardoantoniod@gmail.com</li>
+                            <li>bernardoantoniod@gmail.com</li>
+                        </ul>
+                        <ul className="footer__nav__ul">
+                            <h3>Teléfonos</h3>
 
-                    <li>
-                        <NavLink to="/">
-                            Inicio
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/nosotros">
-                            Nosotros
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/servicios">
-                            Servicios
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/proyectos">
-                            Proyectos
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/cotiza">
-                            Cotiza
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/contacto">
-                            Contacto
-                        </NavLink>
-                    </li>
-                </ul>
-
-                <ul className="footer__nav__ul">
-                    <h3>Contactanos</h3>
-
-                    <li>
-                        <NavLink to="/">
-                            mail
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/nosotros">
-                            telefono
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/servicios">
-                            Direccion
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/proyectos">
-                            Horario
-                        </NavLink>
-                    </li>
-                </ul>
-
-                <ul className="footer__nav__ul">
-                    <h3>Servicios</h3>
-
-                    <li>
-                        <NavLink to="/">
-                            Pruebas SAT
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/nosotros">
-                            Confección de mufas
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/servicios">
-                            Termografias
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/proyectos">
-                            Montaje en BT y AT
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/cotiza">
-                            Automatización y Control
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/contacto">
-                        Mantención
-                        </NavLink>
-                    </li>
-                </ul>
-
-                <ul className="footer__nav__ul">
-
-                    <h3>Nuestras redes</h3>
-
-                    <a className="footer__solcial__logo" href="https://www.instagram.com/hvtest.cl/" target="_blank" rel="noopener noreferrer">
-                        <BsInstagram className="insta-logo"/>
-                    </a>
-                    <a className="footer__solcial__logo" href="https://www.linkedin.com/company/hv-test" target="_blank" rel="noopener noreferrer">
-                        <BsLinkedin className="linke-logo"/>
-                    </a>
-                    <a className="footer__solcial__logo" href="" target="_blank" rel="noopener noreferrer">
-                        <BsWhatsapp className="whatsapp-logo"/>
-                    </a>
-                </ul>
-
-                <NavLink className="a-logo-spa" to="/">
-                    <img className="logo__hvt--main" src="../../../src/assets/LogoHVTest&diagnosticsSpa.png" alt="HVTest-company-logo" />
-                </NavLink>
-
-            </nav>
+                            <li>(56 2)2 384 8000</li>
+                            <li>(56 2)2 384 8000</li>
+                        </ul>
+                    </section>
+                </section>
+                <section className="botton__rights">
+                    <ul>
+                        <li>Copyright 2023 G&N Brands</li>
+                        <li>Contenido corporativo intern. Prohibida su reproducción.</li>
+                    </ul>
+                    <ul>
+                        <li>Politicas de Privacidad y Términos de uso</li>
+                    </ul>
+                    <ul>
+                        <li>Politicas Corporativas</li>
+                    </ul>
+                    <ul>
+                        <li>Mapa de ubicación</li>
+                    </ul>
+                </section>
+            </section>
         </footer>
     )
 }
