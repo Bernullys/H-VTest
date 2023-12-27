a3f68d959f8ba235d08209cd5d922ed49d7ace10
badr <bernardoantoniod@gmail.com>
2023-12-26 21:10:51 -0300

    PersonalCards

--- HVTest/src/Pages/Us/Us.jsx
+++ HVTest/src/Pages/Us/Us.jsx
@@ -1,7 +1,22 @@
 import Layout from "../../Components/Layout/Layout";
 import { BsInstagram, BsLinkedin, BsWhatsapp} from "react-icons/bs"
-
 import "./Us.css"
+import PersonalCards from "./PersonalCards";
+
+const workersInfo = [
+    {
+        name: "Jose Manuel Freitez",
+        position: "Ingeniero Eléctricista",
+        profileImage: "../../../src/assets/dere-1.jpg",
+        linkTolinkedin: "https://www.linkedin.com/in/jos%C3%A9-manuel-freitez-martos-932190bb/"
+    },
+    {
+        name: "Leonardo Alejandro Nuñez",
+        position: "Ingeniero Eléctricista",
+        profileImage: "../../../src/assets/dere-1.jpg",
+        linkTolinkedin: "https://www.linkedin.com/in/leonardo-alejandro-nu%C3%B1ez-araujo-0a465979/"
+    }
+]
 
 const Us = () => {
     return (
@@ -28,20 +43,17 @@ const Us = () => {
                     <p>Estamos conformados por profesionales de alto grado de experiencia en el mercado eléctrico del país, presentes en faenas de importantes empresas del rubro eléctrico, especialistas en el diagnóstico, mantención, resolución de problemas y soluciones integrales en el ámbito eléctrico, industrial y de manufactura. Nuestro servicio profesional y técnico está comprometido en la entrega de un servicio de calidad con altos estándares de calidad y seguridad. </p>
                 </section>
                 <section className="us__middle__section--cards--container">
-                    <article className="personal__card__container">
-                        <section className="image__card__container">
-                            <img src="" alt="" />
-                        </section>
-                        <section className="text-content__card__container">
-                            <h3>Jose Manuel Freitez</h3>
-                            <h4>Ingeniero Eléctricista</h4>
-                            <section className="us__social__logos__container--personal-card">
-                                <a className="footer__solcial__logo" href="https://www.linkedin.com/company/hv-test" target="_blank" rel="noopener noreferrer">
-                                    <BsLinkedin className="card-linke-logo"/>
-                                </a>
-                            </section>
-                        </section>
-                    </article>
+                    {
+                        workersInfo.map(worker => (
+                            <PersonalCards
+                            key={worker.name}
+                            name={worker.name}
+                            position={worker.position}
+                            profileImage={worker.profileImage}
+                            linkTolinkedin={worker.linkTolinkedin}
+                            />
+                        ))
+                    }
                 </section>
             </section>
         </Layout>
